4264870113a4248ba750865ec14d1d22726a6706
brunoamadormdc <brunoamadormdc@gmail.com>
2021-10-03 14:33:17 -0300

    line change

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from app.scrap_routes import scrap_routes
 
 app = flask.Flask(__name__)
+
 cors = CORS(app, origins='*')
 
 app.register_blueprint(scrap_routes, url_prefix='/scrap')
